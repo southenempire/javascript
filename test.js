79438f9c1218d8f08cdbd8b0e0964aed3c05dcd1
Charles-Duke Alaba Naomi <alabanaomi63@yahoo.com>
2022-07-08 14:10:14 +0100

    javascript lessons

---
+++ test.js
@@ -0,0 +1,163 @@
+const ruth = 3 * 1;
+const hood = "Rivers";
+const isABigGirl = true;
+
+console.log(ruth);
+
+console.log( hood);
+
+console.log(isABigGirl);
+
+console.log ('number ' + ruth + ' big ' +hood);
+
+console.log(`no ${ruth} ${hood} road ${isABigGirl} `)
+
+
+let age = "19";
+
+age = "22" 
+
+console.log(age);
+
+//Arrays
+const names = ['naomi','benny','dac',3,6,7,'aany']
+
+console.log(names)
+
+
+
+
+
+//object{}
+
+let person ={
+firstName:  "Chidima",
+LastName: "SugarDaddy",
+age :21,
+isMarried: true,
+children: ["chi", "joy" ,"amaka" ,"caty hannel" ]
+
+}
+
+console.log(person)
+
+const nameOfArray =[
+
+{firstName:  "Chidima",
+LastName: "SugarDaddy",
+age :21,
+isMarried: true,},
+
+{firstName:  "Chi",
+LastName: "Daddy",
+age : 26,
+isMarried: false,
+grandkids:["jon","james","ken","peter"]
+},
+
+{firstName:  "dima",
+LastName: "Sugar",
+age :21,
+isMarried: true,},
+
+]
+console.log(nameOfArray[1].LastName)
+
+
+
+
+//while loop
+
+let x = 0;
+
+while (x<10){
+
+console.log(x)
+x++
+
+}
+console.log
+
+
+
+const restingPeople =['victor','nobody','chi','joy','favour']
+
+for(let x =0; x < restingPeople.length; x++){
+	console.log(restingPeople[x])
+}
+
+
+let victor= 'male' 
+
+if(victor=='male'){
+	console.log('victor is male');
+}
+else{
+	console.log('victor is a female')
+}
+
+
+let naomi = '00'
+
+if (naomi === 00){
+	console.log('naomi is a female')
+}
+else{
+	console.log('naomi is male')
+}
+
+
+let z = 10;
+let y = 12;
+if(z < 10 && y > 10){
+	console.log('Welcome')
+}
+else{
+	console.log('Goodbye')
+}
+
+
+function childOfGod(){
+	console.log('God is Good')
+}
+
+childOfGod();
+
+
+function childOfGod(){
+for (let x = 0; x < 400 ; x++){
+	console.log(`We are done, leave this class now`)
+}
+	}
+
+	childOfGod();
+
+	function name(){
+		console.log('Charles-Duke Alaba Naomi')
+	}
+
+	name();
+
+	const str = "my name is naomi"
+
+	console.log(str[0].toUpperCase() + str.slice(1))
+
+	// fuction
+
+	function myFunc(){
+
+	}
+const myFunc2 = function(){
+
+} 
+
+const myFunc3 = (name,day) => {
+	return greet = `Good ${day} ${name}`
+}
+console.log(myFunc3('souther','morning'))
+
+const percentage = (percent,num)=>{
+	return percentage/100 * num 
+}
+console.log(percentage(15,800))
+   
